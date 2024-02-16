81a4dfdc1b22797dd613747378841480c7f6a625
Natalia Quintero <nataliaqb.de@gmail.com>
2024-02-16 09:44:03 +0100

    creating repo for game

---
+++ rock_paper_scissors.js
@@ -0,0 +1,11 @@
+// Create a variable with the options that the user has
+const options = ['rock', 'paper', 'scissors'];
+// to select one option we need the a random index based on the lenght of the array
+let indexNumber = Math.floor(Math.random() * options.length);
+let userHand = options[indexNumber]
+
+
+
+
+
+console.log(userHand);
