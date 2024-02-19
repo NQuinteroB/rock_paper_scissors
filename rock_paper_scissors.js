b66830e861301e525c40f3d3e0ce6cfc737b9e8b
Natalia Quintero <nataliaqb.de@gmail.com>
2024-02-19 12:15:25 +0100

    function to play the game and return the result

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -12,23 +12,34 @@ const getUserHand = (option) => {
   }
 };
 
+// function to determine an option from the options array
 function getComputerHand() {
   let randomIndex = Math.floor(Math.random() * options.length);
   return options[randomIndex];
 }
 
-
-function determineWinner(userHand, ComputerHand) {
-  if (userHand === ComputerHand) {
+// function to determine the winner using logical operators
+function determineWinner(userHand, computerHand) {
+  if (userHand === computerHand) {
     return "It's a tie";
   }
   if (
-    (userHand === 'rock' && ComputerHand === 'scissors') ||
-    (userHand === 'paper' && ComputerHand === 'rock') ||
-    (userHand === 'scissors' && ComputerHand === 'paper' )
+    (userHand === 'rock' && computerHand === 'scissors') ||
+    (userHand === 'paper' && computerHand === 'rock') ||
+    (userHand === 'scissors' && computerHand === 'paper' )
     ) {
     return 'user won';
   } else {
     return 'computer won';
   }
 };
+
+// function to start the game and calling the other functions
+function playGame(userChoice) {
+  userChoice = getUserHand(userChoice);
+  computerHand = getComputerHand();
+  winner = determineWinner(userChoice, computerHand)
+  return winner
+}
+
+playGame('rock');
